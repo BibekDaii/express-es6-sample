cd66ca0b08f2959b6e77a92b35d5049cd74e02dc
BibekDaii <errorbmhj@gmail.com>
2025-10-26 18:15:45 +0800

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,11 +2,14 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
 
-var app = express();
+const app = express();
+
+// View engine setup
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,9 +20,29 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// Custom route for student ID
+// CUSTOM STUDENT ID ROUTE
 app.get('/student', (req, res) => {
   res.send('My Student ID is 21834808. Welcome to Express');
+});
+
+// ERROR HANDLERS
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// LISTEN ON PORT 8080 (EB REQUIREMENT)
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
 
 export default app;
